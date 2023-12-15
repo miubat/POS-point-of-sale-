5feb291f48ed1a04e26b06693c720b9fa66ca8a0
Sanzida Khan <96487335+miubat@users.noreply.github.com>
2023-12-15 23:47:09 +0600

    php-action

---
+++ php_action/createProduct.php
@@ -0,0 +1,47 @@
+<?php 	
+
+require_once 'core.php';
+
+$valid['success'] = array('success' => false, 'messages' => array());
+
+if($_POST) {	
+
+	$productName 		= $_POST['productName'];
+  // $productImage 	= $_POST['productImage'];
+  $quantity 			= $_POST['quantity'];
+  $rate 					= $_POST['rate'];
+  $brandName 			= $_POST['brandName'];
+  $categoryName 	= $_POST['categoryName'];
+  $productStatus 	= $_POST['productStatus'];
+  $productsupplier	= $_POST['supplier'];
+  $cost	= $_POST['cost'];
+
+	$type = explode('.', $_FILES['productImage']['name']);
+	$type = $type[count($type)-1];		
+	$url = '../assests/images/'.uniqid(rand()).'.'.$type;
+	if(in_array($type, array('gif', 'jpg', 'jpeg', 'png', 'JPG', 'GIF', 'JPEG', 'PNG'))) {
+		if(is_uploaded_file($_FILES['productImage']['tmp_name'])) {			
+			if(move_uploaded_file($_FILES['productImage']['tmp_name'], $url)) {
+				
+				$sql = "INSERT INTO product (product_name, product_image, brand_id, categories_id, quantity, rate, active, status,supplier_id,pro_cost) 
+				VALUES ('$productName', '$url', '$brandName', '$categoryName', '$quantity', '$rate', '$productStatus', 1,'$productsupplier','$cost')";
+
+				if($connect->query($sql) === TRUE) {
+					$valid['success'] = true;
+					$valid['messages'] = "Successfully Added";	
+				} else {
+					$valid['success'] = false;
+					$valid['messages'] = "Error while adding the members";
+				}
+
+			}	else {
+				return false;
+			}	// /else	
+		} // if
+	} // if in_array 		
+
+	$connect->close();
+
+	echo json_encode($valid);
+ 
+} // /if $_POST
